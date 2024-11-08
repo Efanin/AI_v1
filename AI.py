27aeb39ae39acfb729871c190280179235ea396b
Efanin <164139862+Efanin@users.noreply.github.com>
2024-11-08 12:22:35 +0300

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,12 @@
+#pip install requests beautifulsoup4 
+#pip install pillow
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+
+query = input("Введите запрос: ")
+response = requests.get(f"https://www.bing.com/images/search?q={query}")
+soup = BeautifulSoup(response.content, 'html.parser').find('img', class_='mimg').get('src')
+print(soup)
+response = requests.get(soup, stream=True)
+Image.open(response.raw).show()
